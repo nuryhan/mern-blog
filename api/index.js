cb8f16f24b2c2f40cd07dd4ae6ed9dce2124b135
nuryhan <70636933+nuryhan@users.noreply.github.com>
2022-02-14 18:45:21 +0500

    Update index.js

--- api/index.js
+++ api/index.js
@@ -17,6 +17,8 @@ mongoose
   .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+   // useCreateIndex: true,
+    // useFindAndModify: true,
   })
   .then(console.log("Connected to MongoDB"))
   .catch((err) => console.log(err));
